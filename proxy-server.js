4a4a49c651adf4975e825be58347f326d7958ce0
Stantheman227 <Ichhabhungeryo@gmail.com>
2024-11-29 18:24:46 +0100

    Added tracking system for incoming IPs

--- proxy-server.js
+++ proxy-server.js
@@ -3,14 +3,43 @@ const app = express();
 const port = 3001;
 const http = require("http");
 
+const ipTracker = new Map(); // {key: clientIP, {address, timestamp, requestCounts[]}}
+
+// Object Constructor for incoming IP requests
+function requestingIP(address, timestamp) {
+  this.address = address;
+  this.timestamp = timestamp;
+  this.requestCounts = [];
+}
+
 app.get("/", (req, res) => {
+  const clientIP = req.ip;
+  const timestamp = Date.now();
+  let clientData = ipTracker.get(clientIP);
+
+  if (!clientData) {
+    // check if IP has already been logged
+    clientData = new requestingIP(clientIP, timestamp);
+    ipTracker.set(clientIP, clientData); // setting up new entry if IP is not known
+  }
+
+  // If IP is already known push the current timestamp in the requestCounts Array
+  clientData.requestCounts.push(timestamp);
+  console.log(
+    `Request from IP: ${clientIP}, total requests: ${clientData.requestCounts.length}`
+  );
+
+  console.dir(ipTracker);
+  console.dir(ipTracker.get(clientIP));
+  console.log(`Logging client Data: ${clientData}`);
+  console.log(`New Request with IP : ${clientIP} and timestamp: ${timestamp}`);
+
   const options = {
     host: "localhost",
     port: 3000,
     path: "/",
     methos: "GET",
   };
-
   const requestToWeb = http.request(options, (response) => {
     // Request from Proxy to Web to get data instead of redir
     let data = "";
@@ -23,8 +52,7 @@ app.get("/", (req, res) => {
     });
   });
 
-  console.dir(`Incoming Request from IP: ${req.ip}`);
-  console.dir(`Type of incoming req.ip is: ${typeof req.ip}`);
+  console.log(`Incoming Request from IP: ${req.ip}`);
 
   requestToWeb.on("error", (error) => {
     console.log("An error", error);
