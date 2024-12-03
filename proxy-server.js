1a8314be8245c7f05845a607d8bb80d4e40dc6e9
Stantheman227 <Ichhabhungeryo@gmail.com>
2024-12-03 21:50:35 +0100

    Added dontenv, added google reCaptcha to proxy server, added reCaptcha verification on formSign in captcha.html.

--- proxy-server.js
+++ proxy-server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3001;
+require("dotenv").config();
 const http = require("http");
+
+app.use(express.urlencoded({ extended: false }));
+
+const recaptchaKey = process.env.recaptchaKey;
 
 const ipTracker = new Map(); // {key: clientIP, {address, timestamp, requestCounts[]}}
 
@@ -13,11 +18,61 @@ function requestingIP(address, timestamp) {
   this.requestCounts = [];
 }
 
+app.post("/verify", (req, res) => {
+  const params = new URLSearchParams({
+    secret: recaptchaKey,
+    response: req.body["g-recaptcha-response"],
+  });
+
+  fetch("https://www.google.com/recaptcha/api/siteverify", {
+    method: "POST",
+    body: params,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.success) {
+        console.log(
+          `Successful captcha, message: ${res.body} You will be redirected to the desired origin page.`
+        );
+
+        const options = {
+          host: "localhost",
+          port: 3000,
+          path: "/",
+          method: "GET",
+        };
+        try {
+          //Request to Webserver if the captcha has been successfully solved
+          const requestToWeb = http.request(options, (response) => {
+            let data = "";
+            response.on("data", (chunk) => {
+              data = data + chunk.toString();
+            });
+            response.on("end", () => {
+              res.send(data);
+              console.log(data);
+            });
+          });
+
+          requestToWeb.on("error", (error) => {
+            console.log("An error", error);
+          });
+          requestToWeb.end();
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        res.json({ captchaSuccess: false });
+      }
+    });
+});
+
 app.get("/captcha", (req, res) => {
   res.sendFile(path.join(__dirname, `captcha.html`));
 });
 
 app.get("/", (req, res) => {
+  console.log(recaptchaKey);
   const RPS_LIMIT = 1;
   const clientIP = req.ip;
   const timestamp = Date.now();
@@ -30,15 +85,8 @@ app.get("/", (req, res) => {
     method: "GET",
   };
 
-  const options_captcha_html = {
-    host: "localhost",
-    port: 3000,
-    path: "/captcha",
-    method: "GET",
-  };
-
+  // check if IP has already been logged
   if (!clientData) {
-    // check if IP has already been logged
     clientData = new requestingIP(clientIP, timestamp);
     ipTracker.set(clientIP, clientData); // setting up new entry if IP is not known
   }
@@ -56,12 +104,11 @@ app.get("/", (req, res) => {
     RPS_LIMIT
   ) {
     console.log(`Too many requests, please solve the captcha to proceed`);
-
     res.redirect("/captcha");
   } else {
+    // If no RPS restrictions are harmed the users might proceed to the origin page
     try {
       const requestToWeb = http.request(options, (response) => {
-        // Request from Proxy to Web to get data instead of redir
         let data = "";
         response.on("data", (chunk) => {
           data = data + chunk.toString();
